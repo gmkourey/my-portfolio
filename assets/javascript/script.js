6c55942c134ac2b79ff7ea4a22341b834d280986
gkourey <grant.kourey@gmail.com>
2018-12-02 11:24:43 -0800

    changed portfolio projects presented

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -26,9 +26,6 @@ document.addEventListener("scroll", function () {
         setTimeout(function() {
             $(".card6").animate({left: "0px"}, 1000);
         }, 1250)
-        setTimeout(function() {
-            $(".card7").animate({left: "0px"}, 1000);
-        }, 1500)
     }
 });
 
